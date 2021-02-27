6e568ca62f20d0b7b171c497885940dbeb2ac8d7
Jiesean <out_frozen@163.com>
2021-02-27 12:08:40 +0800

    fix: fix mid Int overflow bug

--- demo/AlgorithmTrain/app/src/main/java/com/jiesean/algorithmtrain/bst/BinarySearch.kt
+++ demo/AlgorithmTrain/app/src/main/java/com/jiesean/algorithmtrain/bst/BinarySearch.kt
@@ -15,7 +15,10 @@ object BinarySearch {
         var l = 0
         var r = array.size - 1
         while (r >= l){
-            var mid = (l + r) /2
+            // 在求解mid的过程中，使用l + r 可能会造成整形溢出问题，需要改为减法
+            // var mid = (l + r) /2
+            //此处注意使用移位操作符的时候，需要使用括号，因为其优先级最低
+            var mid = l + ((r - l) shr 1)
 
             if(array[mid] == target){
                 return mid
